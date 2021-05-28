d7f3f7428039a21760dd4a285dcc17db4fe9a9de
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-05-29 00:41:33 +0100

    Initialise Project

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// Write Your Javascript Code here
+// Your task is to check
+// whether an user is trying to add 
+// an empty todo into the list
+// Also add a search bar to search throygh the todo-list
