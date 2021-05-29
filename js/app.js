887cfa6f8213b6b027dad3e0da25f53b33899bc3
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-05-29 01:17:22 +0100

    Creating todo div

--- js/app.js
+++ js/app.js
@@ -1,5 +1,37 @@
 // Write Your Javascript Code here
+
+const input = document.querySelector('.todo-input');
+const todoBtn = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    // console.log('clicked');
+
+    // Create div element
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const todoLi = document.createElement('li');
+    todoLi.innerText = "Make content for page";
+    todoLi.classList.add('.todo-item');
+
+    const btnCheck = document.createElement('button');
+    btnCheck.innerHTML = `<i class="fas fa-check"></i>`;
+    btnCheck.classList.add('.check');
+
+    const btnTrash = document.createElement('button');
+    btnTrash.innerHTML = `<i class="fas fa-trash"></i>`;
+    btnTrash.classList.add('.trash');
+
+    todoDiv.append(todoLi, btnCheck, btnTrash);
+
+    // Appending div into todo list
+    todoList.appendChild(todoDiv);
+});
+
+
 // Your task is to check
 // whether an user is trying to add 
 // an empty todo into the list
-// Also add a search bar to search throygh the todo-list
+// Also add a search bar to search through the todo-list
